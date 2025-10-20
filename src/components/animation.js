792a46bba0bb1b3ebb0054e42b67bdbbf197c7d6
AlexTznDev <alex.tuysuzian91@gmail.com>
2025-10-20 17:42:41 +0200

    Update

--- src/components/animation.js
+++ src/components/animation.js
@@ -1,11 +1,2 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { Reeller } from 'reeller';
-Reeller.registerGSAP(gsap);
-gsap.registerPlugin(ScrollTrigger);
-
 window.Webflow ||= [];
-window.Webflow.push(() => {
-
-});
+window.Webflow.push(() => {});
