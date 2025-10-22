d51de9928cb2aba593acb7f64e1059069c9dc930
AlexTznDev <alex.tuysuzian91@gmail.com>
2025-10-22 15:35:27 +0200

    Update

--- src/components/animation.js
+++ src/components/animation.js
@@ -20,4 +20,23 @@ window.Webflow.push(() => {
       }
     });
   });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  let tlFounders = gsap.timeline({
+    defaults: { ease: 'power2.out' },
+    scrollTrigger: {
+      trigger: '.container_section-allow-anim',
+      start: 'top top',
+      end: '80% 50%',
+      scrub: 2,
+      markers: false,
+    },
+  });
+
+  tlFounders
+    .fromTo('.founders_img-contain', { top: '-30%' }, { top: '40%', duration: 2 })
+    .to('.founders_img-wrap', { scale: 0.7, duration: 2, ease: 'power1.out' }, '<')
+    .to("[founder-img-wrap='circle-gold']", { opacity: 1, duration: 2 }, '<')
+    .to("[founder-img-wrap='circle']", { opacity: 0, duration: 2 }, '<');
 });
