d238015e632984a3b8b0830cbfbc5c1a2e5a50fb
AlexTznDev <alex.tuysuzian91@gmail.com>
2025-10-22 17:14:10 +0200

    Update

--- src/components/animation.js
+++ src/components/animation.js
@@ -33,15 +33,15 @@ window.Webflow.push(() => {
     scrollTrigger: {
       trigger: '.container_section-allow-anim',
       start: 'top top',
-      end: '80% 50%',
+      end: '+=800px',
       scrub: 2,
-      markers: false,
+      markers: true,
     },
   });
 
   tlFounders
-    .fromTo('.founders_img-contain', { top: '-30%' }, { top: '40%', duration: 2 })
+    .fromTo('.founders_img-contain', { top: '-26rem' }, { top: '36rem', duration: 2 })
     .to('.founders_img-wrap', { scale: 0.7, duration: 2, ease: 'power1.out' }, '<')
-    .to("[founder-img-wrap='circle-gold']", { opacity: 1, duration: 2 }, '<')
+    .to("[founder-img-wrap='circle-gold']", { opacity: 0.5, duration: 2 }, '<')
     .to("[founder-img-wrap='circle']", { opacity: 0, duration: 2 }, '<');
 });
